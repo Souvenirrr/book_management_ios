8fbcc4cbc2939bae9578ce2381d51db15720a51e
rs-tungtc <tungtc@runsystem.net>
2021-12-27 16:47:27 +0700

    first commit

--- BookManagement/BooksController.swift
+++ BookManagement/BooksController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeController.swift
 //  BookManagement
 //
 //  Created by tung on 27/12/2021.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
-}
-
+ }
---
+++ BookManagement/Views/Login/LoginController.swift
@@ -0,0 +1,8 @@
+//
+//  LoginController.swift
+//  BookManagement
+//
+//  Created by tung on 27/12/2021.
+//
+
+import Foundation
